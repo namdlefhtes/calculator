7964697561dd52f069cb8b146b1d4bfa3b5ba207
namdlefhtes <namdlefhtes@gmail.com>
2020-11-25 17:27:21 -0500

    prog

---
+++ script.js
@@ -0,0 +1,37 @@
+/* function that grabs value for each button clicked and writes it to tom in display div.
+likely requires object to store each value to avoid have one function per button. 
+
+check 
+
+https://www.theodinproject.com/courses/foundations/lessons/calculator
+
+*/
+
+//try to store each value in array.
+
+let num = [1,2,3];
+
+function add() {
+    let result = num[0] + num [1];
+    console.log(result); 
+}
+
+function subtract() {
+    let result = num[0] - num [1];
+    console.log(result); 
+}
+
+function multiply() {
+    let result = num[0] * num [1];
+    console.log(result); 
+}
+
+function divide() {
+    let result = num[0] / num [1];
+    console.log(result); 
+}
+
+function operate() {
+    
+
+}
