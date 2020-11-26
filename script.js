ee8d8c1e6710e30c2cc3b86b8391549b09b1d3a8
Seth <namdlefhtes@gmail.com>
2020-11-26 16:07:37 -0500

    prog

--- script.js
+++ script.js
@@ -9,29 +9,36 @@ https://www.theodinproject.com/courses/foundations/lessons/calculator
 
 //try to store each value in array.
 
-let num = [1,2,3];
+//let num = [0,1,2,3,4,5,6,7,8,9];
+
+
+function getNumber(num) {
+    console.log(num);
+    let enteredNum = ["temp1","temp2"];
+    enteredNum[0]=num;
+    enteredNum[1]=num;
+    console.log(enteredNum)
+   
+};
 
 function add() {
+    let operator = "add";
     let result = num[0] + num [1];
     console.log(result); 
-}
+    console.log(operator);
+};
 
 function subtract() {
     let result = num[0] - num [1];
     console.log(result); 
-}
+};
 
 function multiply() {
     let result = num[0] * num [1];
     console.log(result); 
-}
+};
 
 function divide() {
     let result = num[0] / num [1];
     console.log(result); 
-}
-
-function operate() {
-    
-
-}
+};
